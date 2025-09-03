b7e81aae4c6d3eb7613cf0f37b8de49117579d9d
Blake Loring <blake@parsed.uk>
2025-09-03 19:20:50 +0800

    Discard for a simpler approach

--- src/to_3d_model.rs
+++ src/to_3d_model.rs
@@ -75,40 +75,6 @@ impl Model {
         for y in 0..heightmap.height {
             for x in 0..heightmap.width {
                 let z = heightmap[(x, y)];
-                let last_z_x = if x == 0 { 0. } else { heightmap[(x - 1, y)] };
-                let next_z_x = if x == heightmap.width - 1 {
-                    0.
-                } else {
-                    heightmap[(x + 1, y)]
-                };
-                let last_z_y = if y == 0 { 0. } else { heightmap[(x, y - 1)] };
-                let next_z_y = if y == heightmap.height - 1 {
-                    0.
-                } else {
-                    heightmap[(x, y + 1)]
-                };
-
-                let last_diag_1 = if x == 0 || y == 0 {
-                    0.
-                } else {
-                    heightmap[(x - 1, y - 1)]
-                };
-                let next_diag_1 = if x == heightmap.width - 1 || y == heightmap.height - 1 {
-                    0.
-                } else {
-                    heightmap[(x + 1, y + 1)]
-                };
-
-                let last_diag_2 = if x == 0 || y == heightmap.height - 1 {
-                    0.
-                } else {
-                    heightmap[(x - 1, y + 1)]
-                };
-                let next_diag_2 = if x == heightmap.width - 1 || y == 0 {
-                    0.
-                } else {
-                    heightmap[(x + 1, y - 1)]
-                };
 
                 let point_to_vertex = |pt, z, lz| {
                     let x = x as f32;
@@ -135,100 +101,23 @@ impl Model {
                         quads.push([pa, pb, pc, pd]);
                     };
 
-                let compute_left_extent =
-                    |quads: &mut Vec<Quad>, first, up_face, down_face, last_z| {
-                        let mut heights = Vec::new();
-
-                        heights.push(z);
-                        heights.push(last_z);
-                        let up = z > last_z;
-
-                        heights.sort_by(f32::total_cmp);
-
-                        let min_z = heights[0];
-                        let max_z = heights[1];
-
-                        extent_z_heights.iter().for_each(|&x| {
-                            heights.push(x);
-                        });
-                        heights.push(last_z_x);
-                        heights.push(next_z_x);
-                        heights.push(last_z_y);
-                        heights.push(next_z_y);
-                        heights.push(last_diag_1);
-                        heights.push(next_diag_1);
-                        heights.push(last_diag_2);
-                        heights.push(next_diag_2);
-
-                        heights.retain(|&x| x >= min_z && x <= max_z);
-
-                        heights.sort_by(f32::total_cmp);
-                        heights.dedup();
-
-                        let face = if up { up_face } else { down_face };
-
-                        for i in 1..heights.len() {
-                            add(&mut *quads, face, heights[i], heights[i - 1]);
-                        }
-                    };
-
-                let compute_right_extent = |quads: &mut Vec<Quad>, face| {
-                    let mut heights = Vec::new();
-
-                    heights.push(z);
-                    heights.push(0.);
-
-                    extent_z_heights.iter().filter(|&&x| x < z).for_each(|&x| {
-                        heights.push(x);
-                    });
-
-                    heights.push(last_z_x);
-                    heights.push(next_z_x);
-                    heights.push(last_z_y);
-                    heights.push(next_z_y);
-
-                    heights.sort_by(f32::total_cmp);
-                    heights.retain(|&x| x <= z);
-                    heights.dedup();
-
-                    for i in 1..heights.len() {
-                        add(&mut *quads, face, heights[i], heights[i - 1]);
-                    }
-                };
-
-                // X face, always draw, reverse order for normal if sloping downward
-                if y == 0 || last_z_y != z {
-                    let up_face = (4, 5, 1, 0);
-                    let down_face = (5, 4, 0, 1);
-                    compute_left_extent(&mut quads, y == 0, up_face, down_face, last_z_y);
-                }
-
-                // Second side (pointing y north), similar shared faces to the x dir
-                if x == 0 || last_z_x != z {
-                    let up_face = (0, 3, 7, 4);
-                    let down_face = (3, 0, 4, 7);
-                    compute_left_extent(&mut quads, x == 0, up_face, down_face, last_z_x);
-                }
-
-                // Third side
-                // Since the previous cell can share an edge we only draw this at the end
-                if y == heightmap.height - 1 {
-                    let face = (3, 2, 6, 7);
-                    compute_right_extent(&mut quads, face);
-                }
-
-                // Fourth side, since the previous cell can share an edge we only draw this at the
-                // end
-                if x == heightmap.width - 1 {
-                    let face = (2, 1, 5, 6);
-                    compute_right_extent(&mut quads, face);
-                }
-
                 // Top
                 add(&mut quads, (0, 1, 2, 3), z, 0.);
 
                 // Base
                 add(&mut quads, (5, 4, 7, 6), z, 0.);
+
+                // X 
+                add(&mut quads, (4, 5, 1, 0), z, 0.);
+                
+                // Y 
+                add(&mut quads, (0, 3, 7, 4), z, 0.);
+
+                // X2
+                add(&mut quads, (3, 2, 6, 7), z, 0.);
+
+                // Y2
+                add(&mut quads, (2, 1, 5, 6), z, 0.);
             }
         }
 
