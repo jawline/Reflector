c5a44d92aa991e317b1923c1c47e7aff53541ec3
Blake Loring <blake@parsed.uk>
2025-09-04 23:41:54 +0800

    Cleaning up the converter

--- src/bin/converter.rs
+++ src/bin/converter.rs
@@ -6,37 +6,17 @@ use rust_las_printer::renderer::to_3d_model;
 use rust_las_printer::to_obj::to_obj;
 use rust_las_printer::to_stl::to_stl;
 use serde_pickle::{DeOptions, SerOptions};
-use std::{
-    fs::{read, File},
-    io::{BufWriter, Write},
-};
+use std::fs::{read, File};
 use threecrate_io::{obj::ObjWriter, MeshWriter};
 
-#[derive(PartialEq, Eq)]
-enum WriteTo {
-    Bin,
+#[derive(PartialEq, Parser, Eq, Debug, Clone)]
+enum Format {
     Stl,
     Obj,
-    UpscaleFmt,
-}
-
-impl WriteTo {
-    fn of_string(s: &str) -> Self {
-        use WriteTo::*;
-        match s {
-            "bin" => Bin,
-            "stl" => Stl,
-            "obj" => Obj,
-            "upscale" => UpscaleFmt,
-            _ => panic!("Unsupported WriteTo format"),
-        }
-    }
 }
 
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
+struct GenerateHeightmap {
     #[arg(short, long)]
     las_folder_path: String,
 
@@ -46,36 +26,49 @@ struct Args {
     #[arg(short, long, default_value_t = 0.25)]
     pixels_per_unit_dim: f32,
 
+    #[arg(short, long, default_value_t = false)]
+    max_y_is_low: bool,
+
+    #[arg(long, default_value_t = 16)]
+    max_threads: usize,
+}
+
+#[derive(Parser, Debug)]
+struct Render {
+    #[arg(short, long)]
+    read_from: String,
+
+    #[arg(short, long)]
+    write_to: String,
+
+    #[clap(subcommand)]
+    format: Format,
+
+    #[arg(short, long, default_value_t = 0.0)]
+    base_depth: f32,
+
     #[arg(short, long, default_value_t = 1)]
     rounds_of_interpolated_hole_filling: usize,
 
     #[arg(short, long, default_value_t = 16)]
     consider_nearest_n_neighbors_for_interpolation: usize,
 
-    #[arg(short, long, default_value_t = false)]
-    max_y_is_low: bool,
-
-    #[arg(long)]
-    write_to: String,
-
-    #[arg(short, long, default_value_t = 0.0)]
-    base_depth: f32,
-
     #[arg(long)]
     override_base_depth_for_tiles_with_no_data: Option<f32>,
-
-    #[arg(long, default_value_t = 1.0)]
-    scale_x: f32,
-    #[arg(long, default_value_t = 1.0)]
-    scale_y: f32,
-    #[arg(long, default_value_t = 1.0)]
-    scale_z: f32,
-
-    #[arg(long, default_value_t = 16)]
-    max_threads: usize,
 }
 
-fn construct_heightmap(limits: &Limits, args: &Args, las_type: &LasType) -> Heightmap<Option<f32>> {
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+enum Args {
+    GenerateHeightmap(GenerateHeightmap),
+    Render(Render),
+}
+
+fn construct_heightmap(
+    limits: &Limits,
+    args: &GenerateHeightmap,
+    las_type: &LasType,
+) -> Heightmap<Option<f32>> {
     let mut streamed = StreamingHeightmap::new(&limits, args.pixels_per_unit_dim);
 
     load_from_directory(
@@ -90,12 +83,12 @@ fn construct_heightmap(limits: &Limits, args: &Args, las_type: &LasType) -> Heig
     streamed.finalize().flip_y()
 }
 
-fn build_terrain_map(limits: &Limits, args: &Args) -> Heightmap<Option<f32>> {
+fn build_terrain_map(limits: &Limits, args: &GenerateHeightmap) -> Heightmap<Option<f32>> {
     let grid_zones = construct_heightmap(&limits, &args, &LasType::GroundAndWater);
     grid_zones
 }
 
-fn build_building_map(limits: &Limits, args: &Args) -> Heightmap<Option<f32>> {
+fn build_building_map(limits: &Limits, args: &GenerateHeightmap) -> Heightmap<Option<f32>> {
     let grid_zones = construct_heightmap(&limits, &args, &LasType::Buildings);
     grid_zones
 }
@@ -116,92 +109,87 @@ fn main() {
 
     let args = Args::parse();
 
-    let write_to = WriteTo::of_string(&args.write_to);
+    match args {
+        Args::GenerateHeightmap(args) => {
+            info!("Producing a sample and preparing for upscaling");
 
-    if write_to == WriteTo::UpscaleFmt {
-        info!("Producing a sample and preparing for upscaling");
+            info!("First pass, collecting limits");
+            let limits = Limits::load_from_directory(&args.las_folder_path, args.max_threads);
 
-        info!("First pass, collecting limits");
-        let limits = Limits::load_from_directory(&args.las_folder_path, args.max_threads);
+            info!(
+                "Bounds: {} {} {} {} {} {}",
+                limits.min_x, limits.max_x, limits.min_y, limits.max_y, limits.min_z, limits.max_z
+            );
 
-        info!(
-            "Bounds: {} {} {} {} {} {}",
-            limits.min_x, limits.max_x, limits.min_y, limits.max_y, limits.min_z, limits.max_z
-        );
+            info!("Main pass, summarizing grid squares");
 
-        info!("Main pass, summarizing grid squares");
+            // We merge the medians for the buildings and the medians for terrain and then make sure
+            // the buildings take precedent. I have found this to be anecdotally better than just a
+            // median since you get less noisy data but don't end up with weird looking underpasses.
+            let mut grid_zones = build_terrain_map(&limits, &args);
+            let buildings = build_building_map(&limits, &args);
+            merge(&mut grid_zones, &buildings);
 
-        // We merge the medians for the buildings and the medians for terrain and then make sure
-        // the buildings take precedent. I have found this to be anecdotally better than just a
-        // median since you get less noisy data but don't end up with weird looking underpasses.
-        let mut grid_zones = build_terrain_map(&limits, &args);
-        let buildings = build_building_map(&limits, &args);
-        merge(&mut grid_zones, &buildings);
+            info!("Flipping the Y axis");
+            let grid_zones = grid_zones.flip_y();
 
-        info!("Flipping the Y axis");
-        let grid_zones = grid_zones.flip_y();
+            let proportion_of_empty_cells = grid_zones.proportion_of_empty_cells();
+            info!("Proportion of empty cells: {}", proportion_of_empty_cells);
 
-        let proportion_of_empty_cells = grid_zones.proportion_of_empty_cells();
-        info!("Proportion of empty cells: {}", proportion_of_empty_cells);
+            if proportion_of_empty_cells > 0.5 {
+                warn!("BAD INPUT: With this upscaling, more than 50% of the pixels are none.");
+            }
 
-        if proportion_of_empty_cells > 0.5 {
-            warn!("BAD INPUT: With this upscaling, more than 50% of the pixels are none.");
-        }
-
-        if grid_zones.proportion_of_empty_cells() > 0.75 {
-            error!("REJECTING INPUT DUE TO HIGH PROPORTION OF ERRORS");
-        } else {
-            let mut file = File::create(args.output_path).unwrap();
-            let min_z = grid_zones.min_z();
-            let max_z = grid_zones.max_z();
-            let grid_zones = grid_zones.normalize_z_by(min_z, max_z);
-            serde_pickle::to_writer(&mut file, &grid_zones, SerOptions::new()).unwrap();
-        }
-    } else {
-        let grid_zones = read(&args.las_folder_path).unwrap();
-        let grid_zones: Heightmap<Option<f32>> =
-            serde_pickle::from_slice(&grid_zones, DeOptions::new()).unwrap();
-        //let grid_zones: Heightmap<Option<f32>> =
-        //    Heightmap::<Option<f32>>::of_nan_as_none(grid_zones);
-        info!("Doing hole filling");
-
-        let grid_zones =
-            (0..args.rounds_of_interpolated_hole_filling).fold(grid_zones, |acc, i| {
-                info!("Neighbor filling round {}", i);
-                acc.interpolate_missing_using_neighbors(
-                    InterpolationMode::Min,
-                    args.consider_nearest_n_neighbors_for_interpolation,
-                )
-            });
-
-        // Here every point will be some
-        info!("Normalizing Z axis");
-        let grid_zones = grid_zones.fill_none_with_zero_and_add_base(
-            args.base_depth,
-            args.override_base_depth_for_tiles_with_no_data
-                .unwrap_or(args.base_depth),
-        );
-
-        match write_to {
-            WriteTo::Stl => {
-                let model = to_3d_model::of_heightmap(&grid_zones, &to_3d_model::Mode::default());
-                let mesh = to_stl(&model);
+            if grid_zones.proportion_of_empty_cells() > 0.75 {
+                error!("REJECTING INPUT DUE TO HIGH PROPORTION OF ERRORS");
+            } else {
                 let mut file = File::create(args.output_path).unwrap();
-                stl_io::write_stl(&mut file, mesh.into_iter()).unwrap()
+                let min_z = grid_zones.min_z();
+                let max_z = grid_zones.max_z();
+                let grid_zones = grid_zones.normalize_z_by(min_z, max_z);
+                serde_pickle::to_writer(&mut file, &grid_zones, SerOptions::new()).unwrap();
             }
-            WriteTo::Obj => {
-                let model = to_3d_model::of_heightmap(&grid_zones, &to_3d_model::Mode::default());
-                let mesh = to_obj(&model);
-                ObjWriter::write_mesh(&mesh, args.output_path).unwrap();
-            }
-            WriteTo::Bin => {
-                let file = File::create(args.las_folder_path).unwrap();
-                let mut writer = BufWriter::new(file);
-                writer
-                    .write(&postcard::to_stdvec::<Heightmap<f32>>(&grid_zones).unwrap())
-                    .unwrap();
-            }
-            WriteTo::UpscaleFmt => unreachable!(),
-        };
+        }
+        Args::Render(args) => {
+            let grid_zones = read(&args.read_from).unwrap();
+            let grid_zones: Heightmap<Option<f32>> =
+                serde_pickle::from_slice(&grid_zones, DeOptions::new()).unwrap();
+            //let grid_zones: Heightmap<Option<f32>> =
+            //    Heightmap::<Option<f32>>::of_nan_as_none(grid_zones);
+            info!("Doing hole filling");
+
+            let grid_zones =
+                (0..args.rounds_of_interpolated_hole_filling).fold(grid_zones, |acc, i| {
+                    info!("Neighbor filling round {}", i);
+                    acc.interpolate_missing_using_neighbors(
+                        InterpolationMode::Min,
+                        args.consider_nearest_n_neighbors_for_interpolation,
+                    )
+                });
+
+            // Here every point will be some
+            info!("Normalizing Z axis");
+            let grid_zones = grid_zones.fill_none_with_zero_and_add_base(
+                args.base_depth,
+                args.override_base_depth_for_tiles_with_no_data
+                    .unwrap_or(args.base_depth),
+            );
+
+            match args.format {
+                Format::Stl => {
+                    let model =
+                        to_3d_model::of_heightmap(&grid_zones, &to_3d_model::Mode::default());
+                    let mesh = to_stl(&model);
+                    let mut file = File::create(args.write_to).unwrap();
+                    stl_io::write_stl(&mut file, mesh.into_iter()).unwrap()
+                }
+                Format::Obj => {
+                    let model =
+                        to_3d_model::of_heightmap(&grid_zones, &to_3d_model::Mode::default());
+                    let mesh = to_obj(&model);
+                    ObjWriter::write_mesh(&mesh, args.write_to).unwrap();
+                }
+            };
+        }
     }
 }
